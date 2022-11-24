ba02c02af7980857f2ccc2549b06a75096057d1e
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-11-24 15:37:52 -0600

    fixed projects.json

--- pages/Projects.js
+++ pages/Projects.js
@@ -1,5 +1,6 @@
 import {Link } from 'next/link';
-import Project from '../projects.json';
+import Project from './Project'
+
 import projects from '../Projects.json'
 
 export default function Projects(){
