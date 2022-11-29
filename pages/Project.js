6442655c692c9cd9ecc070f4ea022f740825e078
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-11-28 20:46:29 -0600

    I need to update the project page and I also need to change the wordings for my summary, all I did was mess up the project page

--- pages/Project.js
+++ pages/Project.js
@@ -3,28 +3,28 @@ export default function Project (props){
     console.log(props)
     return(
        
-            <a className=' flex ' href={`${props.project.url}`} >
-            <div className="flex justify-between flex-col  rounded hover:text-gray-800  hover:bg-white -[#412AC1] bg-gray-800 text-white drop-shadow-xl hover:drop-shadow-8xl flex-wrap m- items-center justify-center">
-                <div className="flex  justify-center">
-                    <div className=' sm:w-96 '>
+            <a className=' flex justify-center xl:w-1/3' href={`${props.project.url}`} >
+                <div className="">
+                   <div className='flex justify-center  '>
+                    <div>
+
              <h2 className='text-center text-2xl  rounded bold m-4'>{props.project.name} </h2> 
-             <div className=' justify-center flex   text-center m2 m2 rounded flex items-center'>
+             <div className=' justify-center flex  sm:w-1/2 text-center m2 m2 rounded flex items-center'>
+                    </div>
             <p className="text-lg   rounded lg:text-xl ">{props.project.description}</p>
             </div>
              <div><br/></div>
             </div>
-              
-                </div>
-                
-                <div className="">
+            {/* <div className=""> */}
                  <img src={""+props.project.media+""}  />
+            {/* </div> */}
+            <div className=" flex justify-center ">
+                    <h2 className=' text-2xl  rounded bold '>{props.project.skills} </h2> 
+                </div> 
                 </div>
-
-                <div className="">
-                    <h2 className='text-center text-2xl  rounded bold m-4'>{props.project.skills} </h2> 
-                </div>
+                 
             
-           </div>
+            
              </a>
         
     )
