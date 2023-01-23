4494143a19eb06641eafc7f6ff5bb54e49926b52
JamesOnwordi <Jamesonwordi50@gmail.com>
2023-01-22 22:58:25 -0700

    ready for updated portfolio

--- pages/Project.js
+++ pages/Project.js
@@ -12,7 +12,7 @@ export default function Project (props){
 <div className="flex justify-center">
     <div className="max-w-">
     <a className='flex drop-shadow-2xl  justify-center ' href={`${props.project?props.project.url:""}`} >
-        <Card  className="hover:bg-white hover:text-gray-700 rounded-none border-radius-none dark:text-gray-300 flex w-fit" imgSrc={""+ props.project?props.project.media:""+ ""} >
+        <Card  className="hover:bg-white hover:text-gray-700 rounded-none border-radius-none dark:text-gray-300 flex w-fit" imgSrc={`${props.project?props.project.media:""}`} >
     
     <p className="font-normal text-xl text-center h-24  ">
     {props.project?props.project.description:""}
